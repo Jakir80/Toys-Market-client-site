d3c48c8c0a51ba5181b16ad23ca9f6db1d630a18
Jakir80 <jh7593617@gmail.com>
2023-05-20 04:20:09 -1100

    create  all toys ok

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -10,7 +10,7 @@ const Gallery = () => {
     return (
         <div>
             <h2 className='text-center bg-gray-200 p-6 rounded-lg text-2xl font-semibold mt-10'>In which toys You can be like from this photo Gallery ?</h2>
-            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 mb-5  rounded-lg border-gray-800 border p-4 mt-20 '>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-5  rounded-lg border-gray-800 border p-4 mt-20 '>
                 {
                     alldetails.map(details =><>
  <div key={details._id} className="flex flex-wrap justify-center">
