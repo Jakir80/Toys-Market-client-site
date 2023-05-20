f59328064644c84945fc4f03a572a46975e89e07
Jakir80 <jh7593617@gmail.com>
2023-05-20 06:51:11 -1100

    hide firebase config

--- src/Components/Firebase/firebase.config.js
+++ src/Components/Firebase/firebase.config.js
@@ -7,12 +7,7 @@ const firebaseConfig = {
     storageBucket:import.meta.env.VITE_storageBucket,
     messagingSenderId:import.meta.env.VITE_messagingSenderId,
     appId:import.meta.env.VITE_appId,
-    // apiKey: "AIzaSyA6VEmzGfslUs5-H22NgzQU11xP96vf7QA",
-    // authDomain: "toys-market-80a22.firebaseapp.com",
-    // projectId: "toys-market-80a22",
-    // storageBucket: "toys-market-80a22.appspot.com",
-    // messagingSenderId: "484842523887",
-    // appId: "1:484842523887:web:e3715b273958c30b1bdf1a"
+   
 };
 
 const app = initializeApp(firebaseConfig);
